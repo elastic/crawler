9b4053123d2a7df6f3e7759e737743ead00c0f51
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2024-05-03 17:51:36 +0200

    Add LICENSE file and headers

--- lib/utility/bulk_queue.rb
+++ lib/utility/bulk_queue.rb
@@ -1,10 +1,10 @@
-# frozen_string_literal: true
-
 #
 # Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-# or more contributor license agreements. Licensed under the Elastic License;
-# you may not use this file except in compliance with the Elastic License.
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
 #
+
+# frozen_string_literal: true
 
 require 'elasticsearch/api'
 
