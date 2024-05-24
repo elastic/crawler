9bfd29f12bb2e768b14f402acef8004bd5446072
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2024-05-24 11:19:03 +0200

    Add faux gem license headers (#27)
    It was decided on Slack that for the vendored Faux gem we should keep
    the original MIT license.
    This PR adds that license as a header to all Faux gem files.

--- vendor/faux/sites/simple_site.rb
+++ vendor/faux/sites/simple_site.rb
@@ -1,3 +1,8 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the MIT License;
+# see LICENSE file in the project root for details
+#
 class SimpleSite < Faux::Base
   page '/' do
     head { atom_to '/feed' }
