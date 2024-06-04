ae6697804cc29e7dba684c57afc45e1623447d77
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2024-06-04 18:34:01 +0200

    Update scripts and README (#32)
    The script files were a bit bloated as we haven't looked at them since
    porting over base crawler. There were also limitations (e.g. forced env
    managers) that I don't think would be healthy for an open-code repo.
    
    - Remove script funcs and script files that are not used
    - Change echo coloring for progress statements from yellow to blue
        - Yellow should be for warnings only 
    - Remove hard requirement for `rbenv` and `jenv` when running `make
    install`
    - Users can opt-in to this by passing in env var
    `CRAWLER_MANAGE_ENV=true`
    - Add currently enabled Java and Ruby versions to bash output when
    running `make install`
    - Remove detailed instructions for env management from README.md
    - Instead we just have a recommendation to use an env manager, and links
    to official docs

--- script/functions.sh
+++ script/functions.sh
@@ -16,32 +16,22 @@ else
   RESET=$(tput sgr0)
 fi
 
-#---------------------------------------------------------------------------------------------------
 function yellow_echo() {
   echo "${YELLOW}${*}${RESET}"
-}
-
-function yellow_echo_date() {
-  yellow_echo "[$(date +"%Y-%m-%d %H:%M:%S")] ${*}"
 }
 
 function red_echo()    {
   echo "${RED}${*}${RESET}"
 }
 
-function red_echo_date() {
-  red_echo "[$(date +"%Y-%m-%d %H:%M:%S")] ${*}"
-}
-
 function green_echo()  {
   echo "${GREEN}${*}${RESET}"
 }
 
-function green_echo_date() {
-  green_echo "[$(date +"%Y-%m-%d %H:%M:%S")] ${*}"
+function blue_echo() {
+  echo "${BLUE}${*}${RESET}"
 }
 
-#---------------------------------------------------------------------------------------------------
 function load_version_constraints() {
   if [[ -z "${PROJECT_ROOT}" ]]; then
     PROJECT_ROOT="$(dirname "${BASH_SOURCE[0]}")/.."
@@ -50,101 +40,34 @@ function load_version_constraints() {
   export BUNDLER_VERSION="$(cat "$PROJECT_ROOT/.bundler-version")"
   export BUNDLER_CONSTRAINT="~> $BUNDLER_VERSION"
 
-  export JRUBY_VERSION="$(cat "$PROJECT_ROOT/.ruby-version")"
+  export RUBY_VERSION="$(cat "$PROJECT_ROOT/.ruby-version")"
   export JAVA_VERSION="$(cat "$PROJECT_ROOT/.java-version")"
-}
 
-#---------------------------------------------------------------------------------------------------
-function rbenv_init() {
-  yellow_echo "Checking if rbenv is installed..."
-  if ! command -v rbenv; then
-    echo "ERROR: rbenv is not installed! Please install it by running 'brew install rbenv' (or use your OS-specific install methods)."
-    exit 2
-  fi
-  green_echo "rbenv: OK"
+  blue_echo "Checking version constraints..."
+  green_echo "Required Ruby version: ${RUBY_VERSION}"
+  green_echo "Required Java version: ${JAVA_VERSION}"
   echo
 
-  yellow_echo "Enabling rbenv support..."
-  eval "$(rbenv init -)"
+  if [ "$CRAWLER_MANAGE_ENV" = "true" ]; then
+    rbenv_init
+    ensure_jruby_installed "$RUBY_VERSION"
+    ensure_bundler_version "$RUBY_VERSION"
+    jenv_init
+    ensure_java_installed "$JAVA_VERSION"
+  fi
+
+  RUNNING_RUBY_VERSION=$(ruby --version)
+  RUNNING_JAVA_VERSION=$(java --version)
+
+  blue_echo "Checking running versions..."
+  echo "Running Ruby version: ${RUNNING_RUBY_VERSION}"
+  echo "Running Java version: ${RUNNING_JAVA_VERSION}"
   green_echo "Done!"
   echo
 }
 
-#---------------------------------------------------------------------------------------------------
-function jenv_init() {
-  yellow_echo "Checking if jenv is installed..."
-  if ! command -v jenv; then
-    echo "ERROR: jenv is not installed! Please install it by running 'brew install jenv' (or use your OS-specific install methods)."
-    exit 2
-  fi
-  green_echo "jenv: OK"
-  echo
-
-  yellow_echo "Enabling jenv support..."
-  eval "$(jenv init -)"
-  green_echo "Done!"
-  echo
-}
-
-#---------------------------------------------------------------------------------------------------
-function nvm_init() {
-  yellow_echo "Checking if nvm is installed..."
-  if [ ! -s "$NVM_DIR/nvm.sh" ]; then
-    echo "WARNING: nvm is not installed! Skipping..."
-    return 0
-  fi
-  green_echo "nvm: OK"
-  echo
-
-  yellow_echo "Enabling nvm support..."
-  set +e
-  source "$NVM_DIR/nvm.sh"
-  nvm use
-  rt=$?
-  if [ $rt -eq 3 ]; then
-    try_then_error "Node version from .nvmrc is not installed" "nvm install"
-  elif [ $rt -ne 0 ]; then
-    red_echo "ERROR: something went wrong with 'nvm use'"
-    exit $rt
-  fi
-  set -e
-  green_echo "Done!"
-  echo
-}
-
-#---------------------------------------------------------------------------------------------------
-function ensure_java_installed() {
-  JAVA_VERSION="$1"
-  yellow_echo "Checking if JAVA $JAVA_VERSION is installed..."
-  set +e
-  if ! jenv prefix; then
-    red_echo "ERROR: Java version $JAVA_VERSION is not installed! Please install it from homebrew or use your OS-specific install methods."
-    echo
-    yellow_echo "If you are on a mac, you may need to add homebrew-installed java to jenv: "
-    echo
-    echo "  jenv add /Library/Java/JavaVirtualMachines/temurin-$JAVA_VERSION.jdk/Contents/Home && jenv rehash"
-    echo
-    exit 2
-  fi
-  set -e
-  green_echo "Done!"
-  echo
-}
-
-#---------------------------------------------------------------------------------------------------
-function ensure_jruby_installed() {
-  JRUBY_VERSION="$1"
-  yellow_echo "Checking if JRuby $JRUBY_VERSION is installed..."
-  if [ -z "$(rbenv versions --bare | grep "^$JRUBY_VERSION")" ]; then
-    try_then_error "JRuby version $JRUBY_VERSION is not installed" "script/setup-rubies"
-  fi
-  green_echo "Done!"
-  echo
-}
-
-#---------------------------------------------------------------------------------------------------
 function check_bundle() {
-  yellow_echo "Checking for missing gems..."
+  blue_echo "Checking for missing gems..."
   if ! bundle check > /dev/null; then
     try_then_error "Bundle is missing gems" "script/bundle"
   fi
@@ -152,36 +75,127 @@ function check_bundle() {
   echo
 }
 
-#---------------------------------------------------------------------------------------------------
-function check_yarn() {
-  yellow_echo "Checking for missing NPM packages..."
-  if ! which yarn; then
-    red_echo "ERROR: yarn is not installed! Please install it by running 'brew install yarn' (or use your OS-specific install methods described here: https://legacy.yarnpkg.com/en/docs/install)."
+function rbenv_init() {
+  blue_echo "Checking if rbenv is installed..."
+  if ! command -v rbenv; then
+    echo "ERROR: rbenv is not installed! Please install it by running 'brew install rbenv' (or use your OS-specific install methods)."
     exit 2
   fi
-  if ! yarn check --integrity > /dev/null; then
-    try_then_error "NPM packages are missing" "yarn install"
+  green_echo "rbenv: OK"
+  echo
+
+  blue_echo "Enabling rbenv support..."
+  eval "$(rbenv init -)"
+  green_echo "Done!"
+  echo
+}
+
+function jenv_init() {
+  blue_echo "Checking if jenv is installed..."
+  if ! command -v jenv; then
+    echo "ERROR: jenv is not installed! Please install it by running 'brew install jenv' (or use your OS-specific install methods)."
+    exit 2
+  fi
+  green_echo "jenv: OK"
+  echo
+
+  blue_echo "Enabling jenv support..."
+  eval "$(jenv init -)"
+  green_echo "Done!"
+  echo
+}
+
+function ensure_java_installed() {
+  JAVA_VERSION="$1"
+  blue_echo "Checking if JAVA $JAVA_VERSION is installed..."
+  set +e
+  if ! jenv prefix; then
+    red_echo "ERROR: Java version $JAVA_VERSION is not installed! Please install it from homebrew or use your OS-specific install methods."
+    echo
+    yellow_echo "If you are on a mac, you may need to add homebrew-installed java to jenv: "
+    echo
+    echo "  jenv add /Library/Java/JavaVirtualMachines/<full-java-version-name>/Contents/Home && jenv rehash"
+    echo
+    exit 2
+  fi
+  set -e
+  green_echo "Done!"
+  echo
+}
+
+function ensure_jruby_installed() {
+  RUBY_VERSION="$1"
+
+  blue_echo "Checking if RBENV_VERSION has a value..."
+  if [ -z "${RBENV_VERSION+x}" ]; then
+    green_echo "RBENV_VERSION is unset, everything looks good!"
+  else
+    if [ "$RBENV_VERSION" != "$RUBY_VERSION" ]; then
+      red_echo "RBENV_VERSION is set in env to version '$RBENV_VERSION', which is incompatible with '$RUBY_VERSION'"
+      red_echo "Please unset this variable and try again."
+      exit 2
+    else
+      yellow_echo "RBENV_VERSION is set in env to '$RBENV_VERSION', which is the same as the version in '.ruby-version'."
+      yellow_echo "This should not cause issues, however if dependency installation fails, please try unsetting this variable."
+    fi
+  fi
+  unset RBENV_VERSION
+  green_echo "Done!"
+  echo
+
+
+  blue_echo "Checking if Ruby $RUBY_VERSION is installed..."
+  if [ -z "$(rbenv versions --bare | grep "^$RUBY_VERSION")" ]; then
+    try_then_error "Ruby version $RUBY_VERSION is not installed" "rbenv install --skip-existing"
   fi
   green_echo "Done!"
   echo
 }
 
-#---------------------------------------------------------------------------------------------------
-function check_git() {
-  yellow_echo "Checking your git tags..."
-  if [ -n "$(git tag | grep -E '^(dal05|v0\.2|show)')" ]; then
-      yellow_echo "Pruning bad tags"
-      git fetch --prune-tags --prune origin
-      yellow_echo "Bad tags pruned"
+function ensure_bundler_version() {
+  RUBY_VERSION="$1"
+
+  blue_echo "Checking bundler version in $RUBY_VERSION..."
+  rbenv shell "$RUBY_VERSION"
+  rbenv rehash
+
+  if ! bundler_installed; then
+    red_echo "No bundler found in $RUBY_VERSION!"
+    install_bundler
+  elif ! check_bundler_version; then
+    red_echo "ERROR: $RUBY_VERSION bundler version does not satisfy the constraint: ${BUNDLER_CONSTRAINT}! Installed version: $(bundler --version)"
+    reinstall_bundler
   fi
+
+  bundler --version
   green_echo "Done!"
+  echo
 }
 
-#---------------------------------------------------------------------------------------------------
+function bundler_installed() {
+  ruby -r bundler -e true 2> /dev/null
+}
+
+function check_bundler_version() {
+  ruby -r bundler -e "exit Gem::Requirement.new('${BUNDLER_CONSTRAINT}').satisfied_by?(Gem::Version.new(Bundler::VERSION))"
+}
+
+function install_bundler() {
+  blue_echo "Installing bundler version ${BUNDLER_VERSION}"
+  gem install bundler --clear-sources --source=https://rubygems.org -v="${BUNDLER_VERSION}"
+}
+
+function reinstall_bundler() {
+  blue_echo "Removing all installed bundler versions and installing bundler version ${BUNDLER_VERSION}..."
+  gem uninstall bundler --force --all --ignore-dependencies --executables
+  echo
+  install_bundler
+}
+
 function try_then_error() {
   ISSUE="$1"
   COMMAND="$2"
-  yellow_echo "$ISSUE. Running: '$COMMAND'"
+  blue_echo "$ISSUE. Running: '$COMMAND'"
   eval "$2"
   rt=$?
   if [[ $rt -ne 0 ]] ; then
@@ -190,7 +204,6 @@ function try_then_error() {
   fi
 }
 
-#---------------------------------------------------------------------------------------------------
 function __install_macosx_dev_deps() {
   local root_dir
 
@@ -202,7 +215,7 @@ function __install_macosx_dev_deps() {
       exit 1
     fi
 
-    # Doesn't matter where the calling script is called from, this is where we at yo
+    # Doesn't matter where the calling script is called from
     root_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../"
 
     # Only run homebrew if dependencies have changed since last time
@@ -217,16 +230,6 @@ function __install_macosx_dev_deps() {
       echo "${RED}If temurin11 (java) is being installed Homebrew may ask you for a password,"
       echo "this is fine as the temurin11 recipe needs to install files outside of /usr/local...${RESET}"
       echo
-
-      if [[ "$(uname -s)" == 'Darwin' ]] && [[ "$(arch)" == *'arm64'* ]]
-      then
-        echo "${RED}⚠️  Warning ⚠️"
-        echo "It looks like you're running on an Apple M1 (nice), so you'll need to install Mailhog manually:${RESET}"
-        echo "${BLUE}$ go get github.com/mailhog/MailHog${RESET}"
-	echo
-        echo "${RED}And run it like so:${RESET}"
-        echo "${BLUE}$ ~/go/bin/MailHog${RESET}"
-      fi
 
       sleep 2
 
