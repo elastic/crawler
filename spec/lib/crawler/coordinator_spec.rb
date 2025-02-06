305f05ad6114c7d28241281f7b31a01929286df4
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2025-02-06 11:41:26 +0100

    Add ingest pipeline for 9.x (#203)
    ### Closes https://github.com/elastic/crawler/issues/201
    
    In ES 9.0 the pipeline `ent-search-generic-ingestion` is being replaced
    by `search-default-ingestion`.
    Because we don't have a dedicated Crawler major version for 9.0, it's
    necessary that Crawler can run on both 8.x and 9.x.
    
    #### Changes
    
    - Add pipeline for 9.x `search-default-ingestion`
    - Determine pipeline by checking response from `/info` during preflight
    check
    - Add tests

--- spec/lib/crawler/coordinator_spec.rb
+++ spec/lib/crawler/coordinator_spec.rb
@@ -80,11 +80,12 @@ RSpec.describe(Crawler::Coordinator) do
         _source: { url: 'https://example.com/outdated' }
       }.stringify_keys
     end
+    let(:build_info) { { version: { number: '8.99.0', build_flavor: 'default' } }.deep_stringify_keys }
 
     before do
       allow(ES::Client).to receive(:new).and_return(es_client)
       allow(es_client).to receive(:bulk)
-      allow(es_client).to receive(:info).and_return(true)
+      allow(es_client).to receive(:info).and_return(build_info)
       allow(es_client).to receive(:delete_by_query).and_return({ deleted: 1 }.stringify_keys)
       allow(es_client)
         .to receive(:paginated_search).and_return([search_result])
