1397ae345c481d1a78afe1c6ae954f8dc598ab24
mattnowzari <matt.nowzari@elastic.co>
2025-03-13 14:02:25 -0400

    Adding function to nest flat YAML + Crawler config now takes precedence over ES config

--- lib/crawler/cli/helpers.rb
+++ lib/crawler/cli/helpers.rb
@@ -23,14 +23,37 @@ module Crawler
         end
       end
 
+      # rubocop:disable Style/SafeNavigation
       def self.load_crawl_config(crawl_config, es_config)
         config = load_yaml(crawl_config)
         unless es_config.nil?
           es_config = load_yaml(es_config)
-          config.merge!(es_config) unless es_config.empty?
+          # deep merge config into es_config to make sure fields in crawler config
+          # are prioritized
+          es_config.deep_merge!(config) unless es_config.nil?
         end
 
-        Crawler::API::Config.new(**config.deep_symbolize_keys)
+        # nest any flat yaml present in the configs
+        nested_config = nest_configs(config)
+
+        Crawler::API::Config.new(**nested_config.deep_symbolize_keys)
+      end
+      # rubocop:enable Style/SafeNavigation
+
+      def self.nest_configs(es_config)
+        nested_config = {}
+        es_config.each do |key, value|
+          all_fields = key.split('.')
+          last_key = all_fields[-1]
+
+          pointer = nested_config
+          all_fields[..-2].each do |field|
+            pointer[field] = {} unless pointer.key?(field)
+            pointer = pointer[field]
+          end
+          pointer[last_key] = value
+        end
+        nested_config
       end
     end
   end
