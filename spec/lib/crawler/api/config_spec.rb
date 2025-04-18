143abd8cf96d49ed76392a27bb03a68cd6f5ca0c
Bill Easton <williamseaston@gmail.com>
2025-04-18 01:48:56 +0000

    Fix typo and add tests for ssl certificate loading

--- spec/lib/crawler/api/config_spec.rb
+++ spec/lib/crawler/api/config_spec.rb
@@ -6,6 +6,8 @@
 
 # frozen_string_literal: true
 
+require 'yaml'
+
 RSpec.describe(Crawler::API::Config) do
   describe '#initialize' do
     let(:domain1) do
@@ -18,7 +20,6 @@ RSpec.describe(Crawler::API::Config) do
     let(:domain2) { { url: 'http://domain2.com' } }
     let(:domains) { [domain1, domain2] }
 
-    # https on port 443, http on port 80
     let(:expected_allowlist) { %W[#{domain1[:url]}:443 #{domain2[:url]}:80] }
     let(:expected_seed_urls) { ["#{domain2[:url]}/"] + domain1[:seed_urls] }
 
@@ -139,7 +140,6 @@ RSpec.describe(Crawler::API::Config) do
       it 'should create a crawl rule for the domain' do
         config = Crawler::API::Config.new(domains:)
 
-        # nil when no crawl rules are configured
         crawl_rules_d1 = config.crawl_rules['http://domain1.com']
         expect(crawl_rules_d1).to be_nil
 
@@ -163,6 +163,108 @@ RSpec.describe(Crawler::API::Config) do
             domains:
           )
         end.to raise_error(ArgumentError, 'Crawl rules for http://domain2.com is not an array')
+      end
+    end
+    context 'when configuring SSL CA certificates' do
+      def expect_x509_certificates(certs)
+        expect(certs).to all(be_a(Java::JavaSecurityCert::X509Certificate))
+      end
+
+      let(:base_params) { { domains: [{ url: 'https://example.com' }] } }
+
+      let(:valid_ca_cert_path) { 'spec/fixtures/ssl/ca.crt' }
+      let(:invalid_ca_cert_path) { 'spec/fixtures/ssl/invalid.crt' }
+
+      let(:expired_cert_path) { 'spec/fixtures/ssl/expired/example.crt' }
+      let(:self_signed_cert_path) { 'spec/fixtures/ssl/self-signed/example.crt' }
+
+      let(:non_existent_cert_path) { '/path/to/non_existent/cert.pem' }
+      let(:unreadable_cert_path) { 'spec/fixtures/ssl/unreadable.crt' }
+
+      let(:valid_ca_cert_content) { File.read(valid_ca_cert_path) }
+      let(:invalid_ca_cert_content) { File.read(invalid_ca_cert_path) }
+
+      let(:yaml_config_path) { 'spec/fixtures/ssl/config_with_cert.yml' }
+
+      it 'defaults to an empty array when no certificates are provided' do
+        config = Crawler::API::Config.new(base_params)
+        expect(config.ssl_ca_certificates).to eq([])
+      end
+
+      it 'accepts an explicitly provided empty array' do
+        config = Crawler::API::Config.new(base_params.merge(ssl_ca_certificates: []))
+        expect(config.ssl_ca_certificates).to eq([])
+      end
+
+      it 'raises ArgumentError if ssl_ca_certificates option is not an array' do
+        expect do
+          Crawler::API::Config.new(base_params.merge(ssl_ca_certificates: 'not-an-array'))
+        end.to raise_error(ArgumentError, 'ssl_ca_certificates must be a list of certificates or paths to certificates')
+      end
+
+      it 'raises ArgumentError if an element within the array is not a string' do
+        expect do
+          Crawler::API::Config.new(base_params.merge(ssl_ca_certificates: [123]))
+        end.to raise_error(
+          ArgumentError,
+          'each entry of ssl_ca_certificates must be a certificate or a path to a certificate, but found: 123'
+        )
+      end
+
+      context 'with certificate content strings' do
+        it 'parses a valid certificate string' do
+          config = Crawler::API::Config.new(base_params.merge(ssl_ca_certificates: [valid_ca_cert_content]))
+          expect(config.ssl_ca_certificates.size).to eq(1)
+          expect_x509_certificates(config.ssl_ca_certificates)
+        end
+
+        it 'raises ArgumentError for an invalid certificate string' do
+          expect do
+            Crawler::API::Config.new(base_params.merge(ssl_ca_certificates: [invalid_ca_cert_content]))
+          end.to raise_error(ArgumentError, /Error while parsing an SSL certificate/)
+        end
+      end
+
+      context 'with certificate file paths' do
+        it 'loads a certificate from a valid file path' do
+          config = Crawler::API::Config.new(base_params.merge(ssl_ca_certificates: [valid_ca_cert_path]))
+          expect(config.ssl_ca_certificates.size).to eq(1)
+          expect_x509_certificates(config.ssl_ca_certificates)
+        end
+
+        it 'loads a certificate from an expired certificate file path' do
+          config = Crawler::API::Config.new(base_params.merge(ssl_ca_certificates: [expired_cert_path]))
+          expect(config.ssl_ca_certificates.size).to eq(1)
+          expect_x509_certificates(config.ssl_ca_certificates)
+        end
+
+        it 'loads a certificate from a self-signed certificate file path' do
+          config = Crawler::API::Config.new(base_params.merge(ssl_ca_certificates: [self_signed_cert_path]))
+          expect(config.ssl_ca_certificates.size).to eq(1)
+          expect_x509_certificates(config.ssl_ca_certificates)
+        end
+
+        it 'raises ArgumentError if the file does not exist' do
+          expect do
+            Crawler::API::Config.new(base_params.merge(ssl_ca_certificates: [non_existent_cert_path]))
+          end.to raise_error(ArgumentError, /Error while loading an SSL certificate .* No such file or directory/)
+        end
+
+        it 'raises ArgumentError if the file is unreadable' do
+          allow(File).to receive(:read).with(unreadable_cert_path).and_raise(Errno::EACCES.new(unreadable_cert_path))
+          expect do
+            Crawler::API::Config.new(base_params.merge(ssl_ca_certificates: [unreadable_cert_path]))
+          end.to raise_error(ArgumentError, /Error while loading an SSL certificate .* Permission denied/)
+        end
+
+        it 'loads certificates from a YAML configuration file' do
+          yaml_config = YAML.load_file(yaml_config_path)
+          certificates_from_yaml = yaml_config['ssl_ca_certificates']
+          config = Crawler::API::Config.new(base_params.merge(ssl_ca_certificates: certificates_from_yaml))
+
+          expect(config.ssl_ca_certificates.size).to eq(3) # Expecting 3 certs from the YAML file
+          expect_x509_certificates(config.ssl_ca_certificates)
+        end
       end
     end
   end
