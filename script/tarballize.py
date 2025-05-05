61fcf5528b4999f0054f3c7619e6b60d965166d9
mattnowzari <matt.nowzari@elastic.co>
2025-05-05 12:12:08 -0400

    Script to help tarball the repo

---
+++ script/tarballize.py
@@ -0,0 +1,18 @@
+import tarfile
+import os.path
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser(prog='tarballize', description='Turn a directory into a gzipped tarball')
+    parser.add_argument('source_directory')
+    parser.add_argument('output_filename')
+    return parser.parse_args()
+
+def make_tarfile(output_filename, source_dir):
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+def main():
+    args = get_arguments()
+    make_tarfile(args.output_filename, args.source_directory)
+main()
