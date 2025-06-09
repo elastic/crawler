30e0117be2bd0f1c0b78a526ec8b565b86b94d79
Matt Nowzari <matt.nowzari@elastic.co>
2025-06-09 15:16:12 -0400

    File sink generates human-readable filenames (#311)
    ### Closes https://github.com/elastic/crawler/issues/298
    
    This PR aims to make the File sink generate human-readable `json` files
    as opposed to the hash values it was previously using. The hash values
    were not human readable, which makes understanding their contents at a
    glance impossible.
    
    Here is an example of the filenames with this change:
    
    ![1033E9FF-73E0-422C-AA31-0610C0FD3A12](https://github.com/user-attachments/assets/75807074-bfac-4211-9890-0f6c4cdab641)
    
    ### Checklists
    
    <!--You can remove unrelated items from checklists below and/or add new
    items that may help during the review.-->
    
    #### Pre-Review Checklist
    - [x] This PR does NOT contain credentials of any kind, such as API keys
    or username/passwords (double check `crawler.yml.example` and
    `elasticsearch.yml.example`)
    - [x] This PR has a meaningful title
    - [x] This PR links to all relevant GitHub issues that it fixes or
    partially addresses
    - If there is no GitHub issue, please create it. Each PR should have a
    link to an issue
    - [x] this PR has a thorough description
    - [x] Covered the changes with automated tests
    - [x] Tested the changes locally
    - [x] Added a label for each target release version (example: `v0.1.0`)
    - [x] Considered corresponding documentation changes

--- lib/crawler/output_sink/file.rb
+++ lib/crawler/output_sink/file.rb
@@ -22,9 +22,32 @@ module Crawler
         FileUtils.mkdir_p(dir)
       end
 
+      def generate_filename_from_url(crawl_result)
+        full_url = crawl_result.url.to_s
+        full_url = full_url.chop if full_url.end_with?('/') # trim tailing slash if present
+
+        filename = full_url
+                   .gsub(/[^a-zA-Z0-9\-_]/, '_') # replace slashes with underscores
+                   .squeeze('_') # remove repetitive underscores
+                   .gsub(/^https?_?(www_)?/, '') # remove scheme and www
+
+        # Most OSes limit filenames to 256 chars, we should truncate if too long
+        if filename.length > 255
+          # 128 + 122 = 250 chars, leaving 6 chars for the file extension (.json)
+          starting_idx = 128
+          ending_idx = filename.length - 122
+          # slice out the middle to retain the domain and more unique paths at the end of the URL
+          filename.slice!(starting_idx..ending_idx)
+        end
+
+        filename
+      end
+
       def write(crawl_result)
         doc = to_doc(crawl_result)
-        result_file = "#{dir}/#{crawl_result.url_hash}.json"
+        document_filename = "#{generate_filename_from_url(crawl_result)}.json"
+        system_logger.debug("Writing crawled document to #{dir}/#{document_filename}")
+        result_file = "#{dir}/#{document_filename}"
         ::File.write(result_file, doc.to_json)
 
         success
